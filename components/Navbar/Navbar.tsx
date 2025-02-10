72b8f218c81d5878a9814c766b2e6fbb1d019ce9
santiagoarista <santiagoaristav@outlook.com>
2025-02-10 00:58:33 -0600

    Made Sidebar interactive with md w screen

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -15,7 +15,7 @@ import { ToggleTheme } from "../ToggleTheme"
 export function Navbar() {
     return (
         <nav className="flex items-center px-2 gap-x-4 md:px-6 justify-between w-full bg-background border-b h-20">
-            <div className="block md:hidden">
+            <div className="block xl:hidden">
                 <Sheet>
                     <SheetTrigger className="flex items-center">
                         <Menu />
