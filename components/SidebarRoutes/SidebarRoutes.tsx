a0bf5592b9e394a03ec8dff2cb6efa2a7d1c9646
santiagoarista <santiagoaristav@outlook.com>
2025-02-15 17:55:32 -0600

    Fixed heigh issue with the sidebar

--- components/SidebarRoutes/SidebarRoutes.tsx
+++ components/SidebarRoutes/SidebarRoutes.tsx
@@ -6,10 +6,9 @@ import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 
 export function SidebarRoutes() {
-
     return (
-        <div className="flex flex-col justify-between h-full">
-            <div>
+        <div className="flex flex-col h-full"> {/* Removed justify-between and added flex-grow */}
+            <div className="flex-1 overflow-y-auto"> {/* Make content area scrollable */}
                 <div className="p-2 md:p-6">
                     <p className="text-slate-500 mb-2">General</p>
                     {dataGeneralSidebar.map((item) => (
@@ -36,17 +35,17 @@ export function SidebarRoutes() {
                 </div>
             </div>
 
-            <div>
-                <div className="text-center p-6">
+            <div className="p-6 mt-4"> {/* Footer section */}
+                <div className="text-center">
                     <Button variant="outline" className="w-full bg-black text-white">
                         Upgrade Plan
                     </Button>
                 </div>
                 <Separator />
-                <footer className="mt-3 p-3 text-center xl:p-0 xl:text-sm ">
+                <footer className="mt-3 text-center xl:text-sm ">
                     2025. All rights reserved. ©
                 </footer>
             </div>
         </div>
-    )
+    );
 }
